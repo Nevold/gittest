51754a5f4c7e49c7d6b2b86abb266dbfd3c86973
Nevold <Nevold@tut.by>
2024-12-16 23:24:49 +0300

    2

--- 1.js
+++ 1.js
@@ -26,3 +26,31 @@ function shuffleChar(str, iterations) {
 }
 
 console.log(shuffleChar('012345', 1));
+
+// function shuffleChar(str, iterations) {
+//   let step = iterations;
+//   let result = str;
+
+//   for (let index = 0; index < step; index += 1) {
+//     let left = '';
+//     let right = '';
+
+//     for (let i = 0; i < str.length; i += 1) {
+//       if (i % 2 !== 0) {
+//         right += result[i];
+//       } else {
+//         left += result[i];
+//       }
+//     }
+
+//     result = `${left}${right}`;
+
+//     if (result === str) {
+//       step = iterations % (iterations - step);
+//       console.log(step);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(shuffleChar('qwerty', 6));
